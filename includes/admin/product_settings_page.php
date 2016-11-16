5216c156a36c420beb4c08a7bb7a728dbcd3d215
Rafael Santos <raf.rsr@gmail.com>
2016-11-16 18:52:47 -0500

    add product id in product settings

--- includes/admin/product_settings_page.php
+++ includes/admin/product_settings_page.php
@@ -52,24 +52,47 @@ add_filter(
     }
 );
 
+add_action(
+    'save_post',
+    function ($post_id) {
+        if (isset($_POST['_wireless_product_id']) && $_POST['_wireless_product_id']) {
+            update_post_meta($post_id, '_wireless_product_id', $_POST['_wireless_product_id']);
+        }
+    }
+);
 /**
  * Force show the pricing group for this new product type
  */
 add_action(
     'woocommerce_product_options_pricing',
     function () {
+        $productIdField = woocommerce_wp_text_input(
+            [
+                'id'            => '_wireless_product_id',
+                'label'         => __('Wireless Product ID'),
+                'desc_tip'      => 'true',
+                'wrapper_class' => 'show_if_wireless',
+                'description'   => __('This product ID is used to communicate with the refill API and make the refill.'),
+            ]
+        );
         echo <<<HTML
 <script>
     jQuery('.options_group.pricing').addClass('show_if_wireless');
     var forceVirtual = function(){
         if (jQuery('#product-type').val() === 'wireless'){
             jQuery('#_virtual').prop('checked', true)
+            jQuery('._sale_price_field').hide();
+            jQuery('#_sale_price').val('');
+            jQuery('#_sku').val('');
             jQuery('#_sold_individually').prop('checked', true)
+        }else{
+            jQuery('._sale_price_field').show();
         }
     };
     jQuery('#_virtual').on('change', forceVirtual);
     jQuery('#product-type').on('change', forceVirtual);
 </script>
+    $productIdField
 HTML;
     }
 );
